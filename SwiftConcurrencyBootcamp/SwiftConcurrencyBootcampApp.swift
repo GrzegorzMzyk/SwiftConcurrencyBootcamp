26437b8f0acbfdb8cb4eb1435830cb7ce2448be6
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-10-15 16:23:01 +0200

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Grzegorz Mzyk on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
