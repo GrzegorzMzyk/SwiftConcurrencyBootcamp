bfcd8e2f39c453ad74c436ad63935e8968a7c7ff
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-10-19 17:35:58 +0200

    async/await - download image

---
+++ SwiftConcurrencyBootcamp/DownloadImageAsync.swift
@@ -0,0 +1,69 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Grzegorz Mzyk on 19/10/2024.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncLoader {
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300
+        else {
+            return nil
+        }
+        return image
+    }
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response , error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
+        }
+        .resume()
+    }
+}
+
+class DownloadImageAsyncModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncLoader()
+    
+    func fetchiImage() {
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+                
+        }
+    }
+}
+
+
+struct DownloadImageAsync: View {
+    @StateObject private var viewModel = DownloadImageAsyncModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:250 , height:250)
+            }
+        }
+        .onAppear(){
+            viewModel.fetchiImage()
+        }
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
